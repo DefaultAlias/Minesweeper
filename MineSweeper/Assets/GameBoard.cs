64422e044aa7407189d88a3b56925cb6ea6d1913
Logan Granberry <logan@schoolstatus.com>
2016-09-22 16:32:12 -0500

    added public properties to scripts and set some defaults

--- MineSweeper/Assets/GameBoard.cs
+++ MineSweeper/Assets/GameBoard.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameBoard : MonoBehaviour {
+
+	public Text timerText;
+	public float timerStart = 0;
+	public bool timerRunning = false;
 
 	// Use this for initialization
 	void Start () {
--- MineSweeper/Assets/Tile.cs
+++ MineSweeper/Assets/Tile.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Tile : MonoBehaviour {
 
+	public bool isBomb = false;       		// will be true if the tile is a bomb
+	public float connectedDistance = 1.0f;  // max distance a neighboring tile can be
+	public int chanceOfBomb = 10;       	// chance of being a bomb (out of 100%)
+	public bool isFlagged = false;     		// if the tile has been flagged as a bomb
+	public bool gameRunning = false;    	// used to determine if the match is running
+	public Color[] numbercolors;      		// a color LUT
+
 	// Use this for initialization
 	void Start () {
 	
