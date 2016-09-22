9d3cd1ce905aa20df46db6a6f359d43f12a86d53
Logan Granberry <logan@schoolstatus.com>
2016-09-22 16:46:24 -0500

    added all scripts, tweaked prefabs, and built game board

--- MineSweeper/Assets/GameBoard.cs
+++ MineSweeper/Assets/GameBoard.cs
@@ -10,11 +10,49 @@ public class GameBoard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		timerText.text = "Time: 0";
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		if(timerRunning){
+			timerText.text = "Time: " + (int)(Time.realtimeSinceStartup - timerStart);
+		}
+	}
+
+	public void StartTimer(){
+		timerRunning = true;
+		timerStart = Time.realtimeSinceStartup;
+		BroadcastMessage("BeginGame");
+	}
+
+	public void StopTimer(){
+		timerRunning = false; 
+		BroadcastMessage("EndGame");
+	}
+
+	public void GameOver(){
+		Tile[] tiles = FindObjectsOfType<Tile>();
+		foreach(Tile t in tiles){
+			if(t.isBomb){
+				t.Reveal(); 
+				StopTimer();
+			}
+		}
+	}
+
+	public void TestWinCondition(){
+		bool pass = true;
+		Tile[] tiles = FindObjectsOfType<Tile>();
+		foreach(Tile t in tiles){
+			if(t.isBomb && !t.isFlagged){
+				pass = false;
+			}
+		}
+
+		if(pass){
+			StopTimer();
+			Debug.Log("YOU ARE WINRAR");
+		}
 	}
 }
